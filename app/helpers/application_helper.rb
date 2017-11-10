058db3203723e57d5877be86534de114b184c3b5
jaro <gerlax@gmx.net>
2017-11-10 13:11:23 +0100

    Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.       # Documentation comment
   def full_title(page_title = '')                     # Method def, optional arg
-    base_title = "Raumpool"  # Variable assignment
+    base_title = "Raumpool Messestadt"  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Implicit return
     else
-      "#{page_title} | #{base_title}"                 # String interpolation
+      "#{base_title} | #{page_title}"                 # String interpolation
     end
   end
 
